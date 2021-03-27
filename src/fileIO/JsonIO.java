f6742ddb3a3a06eff82f48ce1a7a95bbe318d43a
Giulia Maraventano <53179989+jouleffect@users.noreply.github.com>
2021-03-27 17:46:05 +0100

    Delete src directory

--- src/fileIO/JsonIO.java
+++
@@ -1,9 +0,0 @@
-package fileIO;
-import org.json.simple.JSONObject;
-/**
- * Questa &egrave; un'interfaccia per le operazioni di input/output su file .json
- */
-@FunctionalInterface
-public interface JsonIO {
-    JSONObject execute();
-}
--- src/filePDF/Fattura.java
+++
@@ -1,9 +0,0 @@
-package filePDF;
-
-/**
- * Questa &egrave; l'interfaccia che estente PDF per la creazione della fattura
- * in PDF
- */
-public interface Fattura extends PDF{
-    void create();
-}
