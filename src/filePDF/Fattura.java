51f686b441fe15cb10ab6c3c61ff5cdbb6b70a30
Giulia Maraventano <53179989+jouleffect@users.noreply.github.com>
2021-03-30 10:02:29 +0200

    Delete src directory

--- src/filePDF/Fattura.java
+++
@@ -1,9 +0,0 @@
-package filePDF;
-
-/**
- * Questa &egrave; l'interfaccia che estente PDF per la creazione
- * e la visualizzazione della fattura in PDF.
- */
-public interface Fattura extends PDF{
-    void create();
-}
--- src/filePDF/Note.java
+++
@@ -1,9 +0,0 @@
-package filePDF;
-
-/**
- * Questa &egrave; l'interfaccia che estente PDF per la creazione
- * e la visualizzazione delle note in PDF.
- */
-public interface Note extends PDF{
-    void create();
-}
